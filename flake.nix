bcddf6bb5fa78493408028e9e78300c444ad4296
Staneesh <blkreasons@outlook.com>
2023-10-03 19:08:26 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -8,24 +8,35 @@
     let 
         system = "x86_64-linux"; 
         pkgs = (import nixpkgs) { inherit system; };
+
+        custom-r = pkgs.rWrapper.override{ 
+            packages = with pkgs.rPackages; [
+                rmarkdown
+            ]; 
+        };
     in
     {
         devShells.${system}.default = pkgs.mkShell {
             buildInputs = with pkgs; [
-                R
+                custom-r
             ];
         };
 
         packages.${system}.main = pkgs.stdenv.mkDerivation rec{
             name = "main";
             src = ./.;
-            buildInputs = with pkgs; [];
+            buildInputs = with pkgs; [ 
+                custom-r 
+            ];
             buildPhase = ''
-                chmod +x main.rmd
+                ${custom-r}/bin/Rscript -e "rmarkdown::render('main.rmd’, 'html_document')"
             '';
             installPhase = ''
+                mkdir -p $out/doc
                 mkdir -p $out/bin
                 cp main.rmd $out/bin
+                chmod +x $out/bin/main.rmd
+                mv main.html $out/doc
             '';
         };
 
